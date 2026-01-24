a0e5711e22f27660eb95e0cdfc9c66da8d82babd
Robert Holst <1259953+roblibob@users.noreply.github.com>
2026-01-24 17:22:46 +0100

    Added cask script

---
+++ Casks/minute.rb
@@ -0,0 +1,19 @@
+cask "minute" do
+  version "0.8"
+  sha256 "8f04ed392aab055c21bf6f1947d15312d99bff941e5ea33a8dc6eecd7a58f1f2"
+
+  url "https://github.com/roblibob/Minute/releases/download/v#{version}/Minute-#{version}.dmg"
+  name "Minute"
+  desc "Local-first meeting recorder with transcription and summaries"
+  homepage "https://github.com/roblibob/Minute"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "Minute.app"
+
+  zap trash: [
+    "~/Library/Application Support/Minute",
+    "~/Library/Preferences/com.roblibob.Minute.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-minute
+
+Homebrew tap for Minute.
+
+## Install
+
+```
+brew tap roblibob/minute
+brew install --cask minute
+```
+
+## Updating
+
+This tap is updated automatically from GitHub Releases.
